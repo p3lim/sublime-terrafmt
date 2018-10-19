374ff1bbf3d2a2d7b3ae4594f492d271e9227b42
Adrian L Lange <mail@p3lim.net>
2018-10-19 17:21:01 +0200

    :lipstick:

--- terrafmt.py
+++ terrafmt.py
@@ -9,10 +9,18 @@ ANSI_ESCAPE = re.compile(r'\x1B\[[0-?]*[ -/]*[@-~]')
 settings = None
 
 def plugin_loaded():
+	'''
+	Called when the plugin is loaded, used to load the settings for the package.
+	'''
 	global settings
 	settings = sublime.load_settings('Terrafmt.sublime-settings')
 
 class Formatter(object):
+	'''
+	Wrapper for the process used to format the Terraform file.
+
+	:param sublime.View view: The view of the file to be formatted.
+	'''
 	def __init__(self, view):
 		self.view = view
 		self.window = view.window()
@@ -24,22 +32,19 @@ class Formatter(object):
 		self.cmd = settings.get('cmd', ['terraform', 'fmt', '-'])
 
 	def format(self, region):
-		# get the region contents
+		'''
+		Attempts to format the code, raising any errors in an output panel if they arise.
+
+		:param sublime.Region region: The region for the file to format
+		:returns: str. Returns the formatted content if no errors arose, else the original content.
+		'''
 		contents = self.view.substr(region)
 
-		# prepare the formatting tool process
-		proc = subprocess.Popen(
-			self.cmd,
-			stdin=subprocess.PIPE,
-			stdout=subprocess.PIPE,
-			stderr=subprocess.PIPE
-		)
-
-		# invoke the process with the contents as stdin, capturing stdout and stderr
-		stdout, stderr = proc.communicate(contents.encode())
-		if stderr or proc.returncode != 0:
+		# run the formatting tool
+		output, error = self._exec(contents)
+		if error:
 			# there was an error, display it
-			self._show_errors(stderr.decode('utf-8'))
+			self._show_errors(error)
 
 			# return the original content
 			return contents
@@ -48,7 +53,28 @@ class Formatter(object):
 		self._hide_errors()
 
 		# return the formatted output
-		return stdout.decode(self.encoding)
+		return output
+
+	def _exec(self, stdin):
+		'''
+		Execute the formatting tool.
+
+		:param str stdin: Stdin for the process, the file content to format
+		:returns: stdout, stderr: Returns the stdout if successful, an empty stdout and error if
+		    unsuccessful.
+		'''
+		proc = subprocess.Popen(
+			self.cmd,
+			stdin=subprocess.PIPE,
+			stdout=subprocess.PIPE,
+			stderr=subprocess.PIPE
+		)
+
+		stdout, stderr = proc.communicate(stdin.encode())
+		if stderr or proc.returncode != 0:
+			return "", stderr.decode('utf-8')
+		else:
+			return stdout.decode(self.encoding), None
 
 	def _show_errors(self, errors):
 		'''
@@ -70,11 +96,21 @@ class Formatter(object):
 		self.window.run_command('hide_panel', {'panel': 'output.terrafmt'})
 
 class TerrafmtCommand(sublime_plugin.TextCommand):
+	'''
+	The `terrafmt` command, invoked by the command palette or before a file save.
+	'''
 	def is_enabled(self):
-		# checks if the file matches the terraform file types
+		'''
+		Checks if the current file viewed is a Terraform file (.tf, .tfvars) or not.
+
+		:returns: bool
+		'''
 		return self.view.score_selector(0, 'source.terraform') != 0
 
 	def run(self, edit):
+		'''
+		Formats the current file viewed, replacing its contents.
+		'''
 		formatter = Formatter(self.view)
 
 		# get the entire view region
@@ -89,5 +125,8 @@ class TerrafmtCommand(sublime_plugin.TextCommand):
 
 class TerrafmtListener(sublime_plugin.EventListener):
 	def on_pre_save(self, view):
+		'''
+		Called before a dirty view is stored to disk, running the `terrafmt` command.
+		'''
 		if settings.get('format_on_save', True):
 			view.run_command('terrafmt')
